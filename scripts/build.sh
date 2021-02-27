ea1144a23b8765296219bc34affcb2d4e3a9896a
jiangWeixian <jiangweixian1994@gmail.com>
2021-02-27 23:42:44 +0800

    🐛 bin name

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,18 @@
+mkdir builds
+
 # macos
 cargo build --release 
-tar -czf target/release/jscpdrs.tar.gz target/release/jscpdrs
-shasum -a 256 target/release/jscpdrs.tar.gz
+mv target/release/jscpdrs builds/jscpdrs
+cd builds
+tar -czf jscpdrs.tar.gz jscpdrs
+shasum -a 256 jscpdrs.tar.gz
+cd ..
+
+echo "$PWD"
 
 # macos x86_64-apple-darwin
 cargo build --release --target=x86_64-apple-darwin
-tar -czf target/x86_64-apple-darwin/release/jscpdrs-x86_64-apple-darwin.tar.gz target/x86_64-apple-darwin/release/jscpdrs
+mv target/x86_64-apple-darwin/release/jscpdrs builds/jscpdrs-cli
+cd builds
+tar -czf jscpdrs-x86_64-apple-darwin.tar.gz jscpdrs-cli
+cd ..
